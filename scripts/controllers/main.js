cee84c9ec25f21cde2795fed01cfefaa8caa9b27
Jordan Johansen <jordanjohan@gmail.com>
2016-02-01 17:13:19 -0800

    Add orderBy and filter directives to request queue.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -5,4 +5,9 @@ gaRequest.controller('mainCtrl', function($scope, dataService) {
         console.log(response.data);
         $scope.requests = response.data;
     });
+
+    $scope.sortType = 'submitDate'; // Set default sort type
+    $scope.sortReverse = false; // Set default sort order
+    $scope.searchRequests = ''; // Set default search/filter term
+
 });
