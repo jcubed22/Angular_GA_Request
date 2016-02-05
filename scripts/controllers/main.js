9f06e2f3f08503607422e2cacf848045bd622176
Jordan Johansen <jordanjohan@gmail.com>
2016-02-04 16:58:43 -0800

    Add comments of dubious usefulness.

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -4,8 +4,11 @@ angular.module('gaRequest')
 .controller('mainCtrl', function($scope, dataService) {
   $scope.dataService = dataService;
   $scope.testRequests = dataService.testRequests;
+  // Tells $scope where to write fields values to?
   $scope.fields = dataService.fields;
 
+  /* For getting the hardcoded requests in /mock/requests.json.
+  To use, change ng-repeat in request-queue.html */
   dataService.getRequests(function(response) {
     $scope.requests = response.data;
   });
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -24,8 +24,9 @@ angular.module('gaRequest')
   };
 
   this.addRequest = function() {
-    //Sets variable equal to current date as formatted by moment.js
+    // Sets variable equal to current date as formatted by moment.js
     var now = moment().format("MM/DD/YYYY");
+    // Formats dueDate form field from datepicker into more readable format.
     var due = moment(fields.dueDate).format("MM/DD/YYYY");
     var response = {
       jobName: fields.jobName,
@@ -38,7 +39,7 @@ angular.module('gaRequest')
       submitDate: now
     };
     this.testRequests.push(response);
-    fields.jobName = null;
+    fields.jobName = null; // Reset form fields to null for next input.
     fields.jobType = null;
     fields.mediaType = null;
     fields.dimensions = null;
@@ -47,6 +48,9 @@ angular.module('gaRequest')
     fields.requestedBy = null;
   };
 
+  /* Passing in index from an ng-repeat with orderBy or a filter
+  results in incorrect items being deleted.  Pass in the item itself
+  (in this case request), and find the index of that item in the array. */
   this.deleteRequest = function(request) {
     var index = this.testRequests.indexOf(request);
     this.testRequests.splice(index, 1);
