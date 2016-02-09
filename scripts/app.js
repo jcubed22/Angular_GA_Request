548a0162a4f7caba0c4a394f9071dcdcc19f0460
Jordan Johansen <jordanjohan@gmail.com>
2016-02-09 15:37:50 -0800

    Removed ui bootstrap, because screw that.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
 // I've also seen var gaRequest = angular.module('gaRequest', []);
 // Is one way better than other?
-angular.module('gaRequest', ['ngAnimate', 'ui.bootstrap']);
+// ngAnimate, ui.bootstrap
+angular.module('gaRequest', []);
